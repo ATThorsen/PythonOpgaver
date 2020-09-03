fee7547f6ac97c4438b18f29813e51125a569646
ATThorsen <thorsen.aske@gmail.com>
2020-09-03 15:48:20 +0200

    week2 end push

---
+++ Week2/Modules/Test.py
@@ -0,0 +1,11 @@
+from utils import *
+
+filesInFolder("/home/jovyan/my_noteBooks/week2")
+
+lst = list_files("/home/jovyan/my_noteBooks")
+
+print(lst)
+
+emails(lst)
+
+markDowns(lst)
--- Week2/Modules/utils.py
+++ Week2/Modules/utils.py
@@ -1,11 +1,12 @@
 import sys
 import os
+from WriteToFile import *
 from pathlib import Path
 
-print("Flie      part", Path(__file__).absolute())
-print("Directory part", Path().absolute())
+#print("Flie      part", Path(__file__).absolute())
+#print("Directory part", Path().absolute())
 
-thePath = sys.argv[1]
+#thePath = sys.argv[1]
 
 def filesInFolder(dir):
     listOfPaths = (os.listdir(dir))
@@ -17,19 +18,21 @@ def filesInFolder(dir):
     with open(output_file, 'w') as file_object:
         file_object.write(myFile)
 
-filesInFolder(thePath)
 
+#filesInFolder(thePath)
+#filesInFolder("/home/jovyan/")
 r = []
-def list_files(dir):                                                                                                                                                                                                              
-    
+def list_files(dir):                                                                                                                                                                                                      
     for root, dirs, files in os.walk(dir):
         for name in files:
+            print(name)
             r.append(os.path.join(root, name))
             print(root + name)
+    return r
             
              
-list_files(thePath)
-
+#list_files(thePath)
+#list_files("/home/jovyan/")
 def firstLine(lst):
     for myFile in lst:
         if myFile.endswith(".txt"):
@@ -39,8 +42,8 @@ def firstLine(lst):
                     print(lines[0])
 
 
-firstLine(r)
-
+#firstLine(r)
+#firstLine(r)
 def emails(lst):
     for myFile in lst:
         if myFile.endswith(".txt"):
@@ -49,14 +52,18 @@ def emails(lst):
                 for line in lines:
                     if(("@")in line): print(line)
 
-emails(r)
-
+#emails(r)
+#emails(r)
 def markDowns(lst):
     for myFile in lst:
         if myFile.endswith(".md"):
             with open(myFile,'r') as file_object:
                 lines = file_object.readlines()
                 for line in lines:
-                    if "#" in line[0] : print(line)
+                    if "#" in line[0]:
+                        markDownList = [line]
+                        write_list_to_file("MarkDownFiles.txt", markDownList)
+                        
 
-markDowns(r)
+#markDowns(r)
+#markDowns(r)
